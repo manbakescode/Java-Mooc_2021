b886763f6a5d71474379d91e9da07bcf4a56c6fd
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-08-22 23:49:52 +0100

    Completed Part04_02

--- TMCProjects/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,15 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+
+        Account shuaibsAccount = new Account("Shuaib's Account", 100);
+
+        System.out.println(shuaibsAccount);
+
+        shuaibsAccount.deposit(20);
+
+        System.out.println(shuaibsAccount);
+
+
     }
 }
