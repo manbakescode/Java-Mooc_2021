c722a2e5f79b2ba7621888bc2b4aec41e5fbd05c
manbakescode <42654011+manbakescode@users.noreply.github.com>
2021-08-24 23:56:25 +0100

    Part_04

---
+++ TMCProjects/mooc-java-programming-i/part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,12 @@
+public class Door {
+
+    //Constructor with no parameters -or could leave and Java create default constructor
+    public Door() {
+
+    }
+
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+
+}
--- TMCProjects/mooc-java-programming-i/part04-Part04_06.Door/src/main/java/Main.java
+++ TMCProjects/mooc-java-programming-i/part04-Part04_06.Door/src/main/java/Main.java
@@ -5,9 +5,8 @@ public class Main {
         // This is an empty main method
         // You can use it to test your Door class. Try the following:
 
-//        Door alexander = new Door();
-//
-//        alexander.knock();
-//        alexander.knock();
+        Door alexander = new Door();
+        alexander.knock();
+        alexander.knock();
     }
 }
